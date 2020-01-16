93ec3f71885ee18c8f1a3d76846d422aa74bc560
Alessandro Di Martino <48263019+KarmaAlex@users.noreply.github.com>
2020-01-16 15:43:11 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,6 +2,10 @@ import tweepy
 import random
 import time
 from pynput import keyboard
+from os import system, sys, getcwd
+import requests
+
+#Reader be warned this code is spaghetti af
 
 #Twitter auth
 #Consumer API and API secret key
@@ -10,14 +14,12 @@ auth = tweepy.OAuthHandler("QLntDiZAtB7CjuFEs4tc1DBbS","vradE4DrnojcheXptysnPIar
 auth.set_access_token("1215687993220042752-4Fsnxjz3ACVE7DYq2VEkNh8OBOPSU2","iWvcU54RUpFhR82ZC8Htni44SZoe8mhKxoFXxmVQ9b2F6")
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-#Attemp login
+#Attemp login to twitter api
 try:
     api.verify_credentials()
-    print("Authentication successful")
+    print("Authentication successful\n")
 except:
     print("Error authenticating")
-    exit()
-
 
 worm_facts = []
 #Try to import the worm facts file
@@ -25,46 +27,71 @@ try:
     worm_facts_file = open("wormed.txt","r")
 except FileNotFoundError:
     print("Could not find file")
-    exit()
 
-#Reads all lines in the text document and copies them to empty list
+#Reads all lines in the text document and copies them to empty list and closes file
 for worm_fact in worm_facts_file.readlines():
     worm_facts.append(worm_fact)
+worm_facts_file.close()
 
 #Declaration of various variables that are needed later
+#For random generation, sets max index
 i = len(worm_facts) - 1
+#Defines time the bot should tweet
 tweet_time_h = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,0]
 tweet_time_m = 0
+#For endless loop
 stop = False
 
-#I don't know what this does but it interrupts the script upon pressing the End key
+#Function that is used in the keyboard listener to interrupt execution and reload worm facts
 def on_press(key):
     global stop
+    global i
+    #Quits program when pressing the End key
     if key == keyboard.Key.end:
-        print("End pressed")
+        print("Quitting...")
         stop = True
         #Stops listener
         return False
+    #Reloads worm facts from file when pressing the Ins key
+    elif key == keyboard.Key.insert:
+        print("Reloading worm facts...")
+        try:
+            worm_facts_file = open("wormed.txt","r")
+        except FileNotFoundError:
+            print("Could not find file")
+        worm_facts.clear()
+        for worm_fact in worm_facts_file.readlines():
+            worm_facts.append(worm_fact)
+        i = len(worm_facts) - 1
+        worm_facts_file.close()
+        print("Printing worm facts:")
+        for worm_fact in worm_facts:
+            print(worm_fact)
 
 #Initiates keyboard listener
 with keyboard.Listener(on_press=on_press) as listener:
     #Starts infinite loop that ends on key press
     while stop == False:
-        print("Running")
         #Gets current time
         seconds = time.time()
         curr_time = time.localtime(seconds)
+        print("Controls:\nEnd Quit bot\nIns reload worm facts and print\n")
+        print(str(curr_time.tm_hour) + ":" + str(curr_time.tm_min) + "\n")
         #If time is equal to tweet time it tweets
         if curr_time.tm_hour in tweet_time_h and curr_time.tm_min == tweet_time_m:
-            #Random fact generation
-            api.update_status(worm_facts[random.randint(0,i)])
-            print("Tweeted")
+            #Keeps trying to tweet until it doesn't encounter the duplicate status error
+            #This might cause the bot to exceed the max amounts of calls to the api, in that case fuck this bot
+            try:
+                #Generate random tweet
+                api.update_status(worm_facts[random.randint(0,i)])
+                print("Tweeted")
+                time.sleep(60)
+            except tweepy.TweepError:
+                print("Status Duplicate, trying again")
         else:
             print("Time is not correct, didn't tweet")
-        #Waits 60 seconds, hopefully won't tweet twice in one minute
-        time.sleep(60)
+            time.sleep(60)
+            print("\n")
+
     #Continues listening
     listener.join()
-
-worm_facts_file.close()
-exit()
